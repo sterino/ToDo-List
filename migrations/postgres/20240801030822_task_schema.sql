8114271cff5501e80ffc3a86b14bebf75cadad90
sterino <egore.chip@gmail.com>
2024-08-01 08:08:58 +0500

    new migrations

--- migrations/postgres/20240801030822_task_schema.sql
+++ migrations/postgres/20240801030822_task_schema.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS tasks (
-                                     id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
+    id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
     title VARCHAR UNIQUE NOT NULL,
     active_at TIMESTAMP UNIQUE NOT NULL,
     status VARCHAR DEFAULT 'active'
-    );
+);
 
 -- +goose StatementEnd
 
